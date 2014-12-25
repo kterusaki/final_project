92f3cd9621a715bf21450cb9efb48b38b5f2cf75
kterusaki <teriyakiboy14@gmail.com>
2014-12-25 11:59:48 -0800

    version before changing database to pg from mysql

--- app/models/youtube.rb
+++ app/models/youtube.rb
@@ -2,6 +2,7 @@ class Youtube
 	attr_accessor :client
 	def initialize
 		#response = Typhoeus.get("https://accounts.google.com/o/oauth2/auth?client_id=750028081068-pg82pmf5vej15asc7ci3d64i8hgsq10g.apps.googleusercontent.com&redirect_uri=http://localhost:3000&scope=https://gdata.youtube.com&response_type=code&approval_prompt=auto&access_type=offline")
+		#?code=4/HRtan8WS5KHUdn9gd5hjnPMNqZxHUbJUEF88nfsXNTY.wvQ3oOS70isb3oEBd8DOtNDFm7gclAI
 		@client = YouTubeIt::Client.new(:dev_key => "AI39si6LXuhlmQ0_F_ugn2FPxqtuvWb5Ad5wPTnpJQG1DDN_nPCcfTMCzIDUhIBZtDUY0qJC4-YRLnbU4NvmpDwprQNKAiX3Hg")
 	end
 
