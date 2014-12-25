2091af661c8e66ff23abca8db4eb06e379cae7cf
kterusaki <teriyakiboy14@gmail.com>
2014-12-25 12:32:28 -0800

    Changed db to pg from mysql, create new db, and added procfile

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141120011157) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "identities", force: true do |t|
     t.integer  "user_id"
     t.string   "provider"
@@ -21,7 +24,7 @@ ActiveRecord::Schema.define(version: 20141120011157) do
     t.datetime "updated_at"
   end
 
-  add_index "identities", ["user_id"], name: "index_identities_on_user_id"
+  add_index "identities", ["user_id"], name: "index_identities_on_user_id", using: :btree
 
   create_table "play_queues", force: true do |t|
     t.datetime "created_at"
@@ -41,7 +44,7 @@ ActiveRecord::Schema.define(version: 20141120011157) do
     t.string   "vid_title"
   end
 
-  add_index "tweets", ["identity_id"], name: "index_tweets_on_identity_id"
+  add_index "tweets", ["identity_id"], name: "index_tweets_on_identity_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -59,7 +62,7 @@ ActiveRecord::Schema.define(version: 20141120011157) do
     t.string   "name"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
